822a33e60f71e078e6472fbf69de27ed0ed6c934
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-01-20 14:31:27 +0100

    wip

--- src/orders/components/OrderReturnPage/utils/index.tsx
+++ src/orders/components/OrderReturnPage/utils/index.tsx
@@ -1,2 +1,16 @@
+import { ReturnLineDataParser } from "./ReturnLineDataParser";
+
 export const getById = (idToCompare: string) => (obj: { id: string }) =>
   obj.id === idToCompare;
+
+export const getItemsWithMaxedQuantities = ({ lines, itemsQuantites }) =>
+  itemsQuantites.data.map(item => {
+    const { id } = item;
+    const line = lines.find(getById(id));
+    const newQuantity = line.quantity - line.quantityFulfilled;
+
+    return ReturnLineDataParser.getLineItem(line, {
+      ...item,
+      initialValue: newQuantity
+    });
+  });
