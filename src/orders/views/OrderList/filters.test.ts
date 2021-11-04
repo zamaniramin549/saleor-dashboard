8c9669c9d49f5ecf1988a13f600d96e367ca6db4
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-11-04 13:33:28 +0100

    Add fixtures

--- src/orders/views/OrderList/filters.test.ts
+++ src/orders/views/OrderList/filters.test.ts
@@ -38,6 +38,14 @@ describe("Filtering URL params", () => {
   const intl = createIntl(config);
 
   const filters = createFilterStructure(intl, {
+    preorder: {
+      active: false,
+      value: false
+    },
+    clickAndCollect: {
+      active: false,
+      value: false
+    },
     channel: {
       active: false,
       value: [
