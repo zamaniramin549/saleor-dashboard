f715304386ee29f40c1928d512e97ded131dc5bc
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-11-07 16:21:56 +0100

    Auth Provider cleanups

--- src/auth/hooks/useAuthProvider.ts
+++ src/auth/hooks/useAuthProvider.ts
@@ -1,83 +1,27 @@
 import { IMessageContext } from "@saleor/components/messages";
 import { APP_DEFAULT_URI, APP_MOUNT_URI, DEMO_MODE } from "@saleor/config";
-import { User } from "@saleor/fragments/types/User";
 import { useAuth, useAuthState } from "@saleor/sdk";
-import {
-  AccountErrorFragment,
-  CreateToken,
-  ExternalAuthenticationUrl,
-  ExternalObtainAccessTokens,
-  MutationSetPasswordArgs,
-  SetPasswordMutation,
-  UserFragment
-} from "@saleor/sdk/dist/apollo/types";
 import {
   isSupported as isCredentialsManagementAPISupported,
   login as loginWithCredentialsManagementAPI,
   saveCredentials
 } from "@saleor/utils/credentialsManagement";
 import ApolloClient from "apollo-client";
-import { FetchResult } from "apollo-link";
-import { MutableRefObject, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useQuery } from "react-apollo";
 import { IntlShape } from "react-intl";
 import urlJoin from "url-join";
 
 import { userDetailsQuery } from "../queries";
+import {
+  ExternalLoginInput,
+  RequestExternalLoginInput,
+  RequestExternalLogoutInput,
+  UserContext
+} from "../types";
 import { UserDetails } from "../types/UserDetails";
 import { displayDemoMessage } from "../utils";
 
-export interface RequestExternalLoginInput {
-  redirectUri: string;
-}
-
-export interface ExternalLoginInput {
-  code: string;
-  state: string;
-}
-
-export interface RequestExternalLogoutInput {
-  returnTo: string;
-}
-
-export interface UseAuthProvider {
-  login: (
-    username: string,
-    password: string
-  ) => Promise<
-    Pick<CreateToken, "csrfToken" | "token"> & {
-      errors: AccountErrorFragment[];
-      user: UserFragment;
-    }
-  >;
-  requestLoginByExternalPlugin: (
-    pluginId: string,
-    input: RequestExternalLoginInput
-  ) => Promise<
-    Pick<ExternalAuthenticationUrl, "authenticationData"> & {
-      errors: AccountErrorFragment[];
-    }
-  >;
-  loginByExternalPlugin: (
-    pluginId: string,
-    input: ExternalLoginInput
-  ) => Promise<
-    Pick<ExternalObtainAccessTokens, "csrfToken" | "token"> & {
-      user: UserFragment;
-      errors: AccountErrorFragment[];
-    }
-  >;
-  logout: () => Promise<void>;
-  setPassword: (
-    opts: MutationSetPasswordArgs
-  ) => Promise<
-    FetchResult<SetPasswordMutation, Record<string, any>, Record<string, any>>
-  >;
-  authenticated: boolean;
-  authenticating: boolean;
-  user?: User;
-  autologinPromise?: MutableRefObject<Promise<any>>;
-}
 export interface UseAuthProviderOpts {
   intl: IntlShape;
   notify: IMessageContext;
@@ -88,7 +32,7 @@ export function useAuthProvider({
   intl,
   notify,
   apolloClient
-}: UseAuthProviderOpts): UseAuthProvider {
+}: UseAuthProviderOpts): UserContext {
   const {
     login,
     getExternalAuthUrl,
